62c82cef0babbe60ca3e0175a34b953d5368340a
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-11-22 12:42:30 +0100

    fix: revert to actual implementation

--- avalanche/evaluation/metrics/accuracy.py
+++ avalanche/evaluation/metrics/accuracy.py
@@ -218,10 +218,7 @@ class AccuracyPluginMetric(GenericPluginMetric[float, Accuracy]):
         return self._metric.result()
 
     def update(self, strategy):
-        if hasattr(strategy, "mb_task_id"):
-            pass
-        else:
-            self._metric.update(strategy.mb_output, strategy.mb_y)
+        self._metric.update(strategy.mb_output, strategy.mb_y)
 
 
 class AccuracyPerTaskPluginMetric(
