a0eb6cc5de702a0f0b1a05a31347abdfe966be04
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-11-22 17:37:08 +0100

    feat: add barlow twins loss

--- avalanche/training/self_supervised_losses.py
+++ avalanche/training/self_supervised_losses.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class SimSiamLoss(nn.Module):
@@ -10,3 +11,25 @@ class SimSiamLoss(nn.Module):
         z1 = z1.detach()
         z2 = z2.detach()
         return -(self.criterion(p1, z2).mean() + self.criterion(p2, z1).mean()) * 0.5
+
+
+class BarlowTwinsLoss(nn.Module):
+    def __init__(self, lambd=5e-3):
+        super().__init__()
+        self.lambd = lambd
+
+    def forward(self, z1, z2):
+        batch_size = z1.size(0)
+
+        # cross-correlation matrix
+        c = z1.T @ z2 / batch_size
+
+        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
+        off_diag = self.off_diagonal(c).pow_(2).sum()
+
+        return on_diag + self.lambd * off_diag
+
+    def off_diagonal(self, x):
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
