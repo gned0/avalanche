32aac74351239301e5eb7226cff962b5589c516c
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2025-01-23 19:23:39 +0100

    fix: parameters

--- avalanche/training/plugins/pfr.py
+++ avalanche/training/plugins/pfr.py
@@ -8,13 +8,12 @@ class PFRPlugin(SelfDistillationPlugin):
     introduced in https://arxiv.org/pdf/2112.15022.
     """
 
-    def __init__(self, output_dim: int = 128, distill_barlow_lamb: float = 0.005):
+    def __init__(self, output_dim: int):
         super().__init__(
             distillation_loss=nn.CosineSimilarity(dim=1),
             output_dim=output_dim,
             hidden_dim=output_dim // 2,
         )
-        self.distill_barlow_lamb = distill_barlow_lamb
 
     def before_backward(self, strategy, **kwargs):
         if self.frozen_backbone is None:
